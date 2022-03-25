614b5676fc81f1b0add26662706770e1120df36c
paulrozhkin <blackiiifox@gmai.com>
2022-03-25 14:23:38 +0300

    006 problem

--- LeetCode.Tests/0-50/006_ZigzagConversion_Test.cs
+++ LeetCode.Tests/0-50/006_ZigzagConversion_Test.cs
@@ -1,6 +1,58 @@
-﻿namespace LeetCode.Tests._0_50
+﻿using LeetCode._0_50;
+using Xunit;
+
+namespace LeetCode.Tests._0_50
 {
     public class _006_ZigzagConversion_Test
     {
+        [Fact]
+        public void ConvertTest_General()
+        {
+            var input = "PAYPALISHIRING";
+
+            var solution = new _006_ZigzagConversion();
+
+            var result = solution.Convert(input, 3);
+            Assert.Equal("PAHNAPLSIIGYIR", result);
+
+            result = solution.Convert(input, 4);
+            Assert.Equal("PINALSIGYAHRPI", result);
+
+            result = solution.Convert(input, 5);
+            Assert.Equal("PHASIYIRPLIGAN", result);
+        }
+
+        [Fact]
+        public void ConvertTest_RowNumberEqualOne()
+        {
+            var input = "PAYPALISHIRING";
+
+            var solution = new _006_ZigzagConversion();
+
+            var result = solution.Convert(input, 1);
+            Assert.Equal(input, result);
+        }
+
+        [Fact]
+        public void ConvertTest_RowNumberEqualZero()
+        {
+            var input = "PAYPALISHIRING";
+
+            var solution = new _006_ZigzagConversion();
+
+            var result = solution.Convert(input, 0);
+            Assert.Equal(input, result);
+        }
+
+        [Fact]
+        public void ConvertTest_EmptyString()
+        {
+            var input = string.Empty;
+
+            var solution = new _006_ZigzagConversion();
+
+            var result = solution.Convert(input, 0);
+            Assert.Equal(input, result);
+        }
     }
 }
--- LeetCode/0-50/006_ZigzagConversion.cs
+++ LeetCode/0-50/006_ZigzagConversion.cs
@@ -1,6 +1,39 @@
-﻿namespace LeetCode._0_50
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode._0_50
 {
     public class _006_ZigzagConversion
     {
+        public string Convert(string s, int numRows)
+        {
+            if (numRows is 0 or 1)
+            {
+                return s;
+            }
+
+            var stringBuilders = new List<StringBuilder>();
+            for (var i = 0; i < numRows; i++)
+            {
+                stringBuilders.Add(new StringBuilder());
+            }
+
+            var isGoingDown = true;
+            var currentRow = 0;
+            foreach (var symbol in s)
+            {
+                stringBuilders[currentRow].Append(symbol);
+                if (currentRow == 0 || currentRow == numRows - 1) isGoingDown = !isGoingDown;
+                currentRow += !isGoingDown ? 1 : -1;
+            }
+
+            var result = new StringBuilder();
+            for (var i = 0; i < numRows; i++)
+            {
+                result.Append(stringBuilders[i].ToString());
+            }
+
+            return result.ToString();
+        }
     }
 }
