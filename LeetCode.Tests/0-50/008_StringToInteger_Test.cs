e76d59fe8964589cae70dff8ead05131f399a0fe
Paul Rozhkin <blackiiifox@gmail.com>
2022-03-29 22:46:34 +0300

    008 problem

---
+++ LeetCode.Tests/0-50/008_StringToInteger_Test.cs
@@ -0,0 +1,161 @@
+﻿using LeetCode._0_50;
+using Xunit;
+
+namespace LeetCode.Tests._0_50
+{
+    public class _008_StringToInteger_Test
+    {
+        [Fact]
+        public void MyAtoiTest_Positive()
+        {
+            var solution = new _008_StringToInteger();
+
+            var result = solution.MyAtoi("123");
+            Assert.Equal(123, result);
+
+            result = solution.MyAtoi("+123");
+            Assert.Equal(123, result);
+        }
+
+        [Fact]
+        public void MyAtoiTest_Nagative()
+        {
+            var solution = new _008_StringToInteger();
+
+            var result = solution.MyAtoi("-123");
+            Assert.Equal(-123, result);
+        }
+
+        [Fact]
+        public void MyAtoiTest_Zero()
+        {
+            var solution = new _008_StringToInteger();
+
+            var result = solution.MyAtoi("0");
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void MyAtoiTest_PositiveZero()
+        {
+            var solution = new _008_StringToInteger();
+
+            var result = solution.MyAtoi("+0");
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void MyAtoiTest_NagativeZero()
+        {
+            var solution = new _008_StringToInteger();
+
+            var result = solution.MyAtoi("-0");
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void MyAtoiTest_Boundary()
+        {
+            var solution = new _008_StringToInteger();
+
+            var result = solution.MyAtoi("2147483647");
+            Assert.Equal(2147483647, result);
+
+            result = solution.MyAtoi("2147483646");
+            Assert.Equal(2147483646, result);
+
+            result = solution.MyAtoi("-2147483648");
+            Assert.Equal(-2147483648, result);
+
+            result = solution.MyAtoi("-2147483647");
+            Assert.Equal(-2147483647, result);
+        }
+
+        [Fact]
+        public void MyAtoiTest_PositiveOverflow()
+        {
+            var solution = new _008_StringToInteger();
+
+            var result = solution.MyAtoi("2147483648");
+            Assert.Equal(int.MaxValue, result);
+
+            result = solution.MyAtoi("2247483647");
+            Assert.Equal(int.MaxValue, result);
+        }
+
+        [Fact]
+        public void MyAtoiTest_NagativeOverflow()
+        {
+            var solution = new _008_StringToInteger();
+
+            var result = solution.MyAtoi("-2147483649");
+            Assert.Equal(int.MinValue, result);
+
+            result = solution.MyAtoi("-2247483648");
+            Assert.Equal(int.MinValue, result);
+        }
+
+        [Fact]
+        public void MyAtoiTest_WhiteSpaceInFrontOfInput()
+        {
+            var solution = new _008_StringToInteger();
+
+            var result = solution.MyAtoi("   123");
+            Assert.Equal(123, result);
+
+            result = solution.MyAtoi("   -123");
+            Assert.Equal(-123, result);
+        }
+
+        [Fact]
+        public void MyAtoiTest_EmptyString()
+        {
+            var solution = new _008_StringToInteger();
+
+            var result = solution.MyAtoi("");
+            Assert.Equal(0, result);
+
+            result = solution.MyAtoi("   ");
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void MyAtoiTest_InvalidInput()
+        {
+            var solution = new _008_StringToInteger();
+
+            var result = solution.MyAtoi("abc");
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void MyAtoiTest_MixInput()
+        {
+            var solution = new _008_StringToInteger();
+
+            var result = solution.MyAtoi("123abc");
+            Assert.Equal(123, result);
+
+            result = solution.MyAtoi("-123abc");
+            Assert.Equal(-123, result);
+        }
+
+        [Fact]
+        public void MyAtoiTest_MixInputWithOverflow()
+        {
+            var solution = new _008_StringToInteger();
+
+            var result = solution.MyAtoi("  2147483648abc");
+            Assert.Equal(int.MaxValue, result);
+
+            result = solution.MyAtoi("  2247483647abc");
+            Assert.Equal(int.MaxValue, result);
+
+            result = solution.MyAtoi("  -2147483649abc");
+            Assert.Equal(int.MinValue, result);
+
+            result = solution.MyAtoi("  -2247483648abc");
+            Assert.Equal(int.MinValue, result);
+        }
+    }
+}
---
+++ LeetCode/0-50/008_StringToInteger.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using static System.Char;
+
+namespace LeetCode._0_50
+{
+    public class _008_StringToInteger
+    {
+        public int MyAtoi(string s)
+        {
+            return new AtoiStateMachine(s).Handle();
+        }
+
+        private class AtoiStateMachine
+        {
+            private string _s;
+
+            private int _stringIndex;
+
+            private State _state;
+
+            private int _sign = 1;
+
+            private int _result = 0;
+
+            public AtoiStateMachine(string s)
+            {
+                _s = s;
+            }
+
+            public int Handle()
+            {
+                while (_state != State.Processed)
+                {
+                    if (_stringIndex == _s.Length)
+                    {
+                        _state = State.Processed;
+                    }
+
+                    switch (_state)
+                    {
+                        case State.WhitespaceProcessing:
+                        {
+                            if (_s[_stringIndex] == ' ')
+                            {
+                                _stringIndex++;
+                            }
+                            else
+                            {
+                                if (IsDigit(_s[_stringIndex]))
+                                {
+                                    _state = State.DigitProcessing;
+                                }
+                                else
+                                {
+                                    if (_s[_stringIndex] == '+' || _s[_stringIndex] == '-')
+                                    {
+                                        _state = State.SignProcessing;
+                                    }
+                                    else
+                                    {
+                                        _state = State.Processed;
+                                    }
+                                }
+                            }
+
+                            break;
+                        }
+                        case State.SignProcessing:
+                        {
+                            if (_s[_stringIndex] == '-')
+                            {
+                                _sign = -1;
+                            }
+
+                            _state = State.DigitProcessing;
+                            _stringIndex++;
+                            break;
+                        }
+                        case State.DigitProcessing:
+                        {
+                            if (IsDigit(_s[_stringIndex]))
+                            {
+                                AddToResult(_s[_stringIndex] - '0');
+                                _stringIndex++;
+                            }
+                            else
+                            {
+                                _state = State.Processed;
+                            }
+
+                            break;
+                        }
+                    }
+                }
+
+                return (_sign * _result);
+            }
+
+            private void AddToResult(int value)
+            {
+                int resultWithSign = _sign * _result;
+                if (resultWithSign > int.MaxValue / 10 || (resultWithSign == int.MaxValue / 10 && value > 7))
+                {
+                    _result = int.MaxValue;
+                    return;
+                }
+
+                if (resultWithSign < int.MinValue / 10 || (resultWithSign == int.MinValue / 10 && value > 8))
+                {
+                    _sign = 1;
+                    _result = int.MinValue;
+                    return;
+                }
+
+                _result = _result * 10 + value;
+            }
+
+            private enum State
+            {
+                WhitespaceProcessing,
+                SignProcessing,
+                DigitProcessing,
+                Processed,
+            }
+        }
+    }
+}
