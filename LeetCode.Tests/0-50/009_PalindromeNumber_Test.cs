e2298dc412026af9a4cf55df06560a4e0790169a
Paul Rozhkin <blackiiifox@gmail.com>
2022-03-29 23:21:21 +0300

    009 problem

---
+++ LeetCode.Tests/0-50/009_PalindromeNumber_Test.cs
@@ -0,0 +1,45 @@
+﻿using LeetCode._0_50;
+using Xunit;
+
+namespace LeetCode.Tests._0_50
+{
+    public class _009_PalindromeNumber_Test
+    {
+        [Fact]
+        public void IsPalindromeTest_SingleDigit()
+        {
+            var solution = new _009_PalindromeNumber();
+            var result = solution.IsPalindrome(0);
+            Assert.True(result);
+
+            result = solution.IsPalindrome(1);
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void IsPalindromeTest_NegativeNumber()
+        {
+            var solution = new _009_PalindromeNumber();
+
+            var result = solution.IsPalindrome(-1);
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void IsPalindromeTest_MultipleDigit()
+        {
+            var solution = new _009_PalindromeNumber();
+            var result = solution.IsPalindrome(11);
+            Assert.True(result);
+
+            result = solution.IsPalindrome(121);
+            Assert.True(result);
+
+            result = solution.IsPalindrome(1231);
+            Assert.False(result);
+
+            result = solution.IsPalindrome(1221);
+            Assert.True(result);
+        }
+    }
+}
