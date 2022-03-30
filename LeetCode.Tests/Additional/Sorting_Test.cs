909157ae4b150cc156eada1c7fb8114b681ecb01
paulrozhkin <blackiiifox@gmai.com>
2022-03-30 18:44:38 +0300

    additional (sorting)

---
+++ LeetCode.Tests/Additional/Sorting_Test.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoFixture;
+using FluentAssertions;
+using LeetCode.Additional;
+using Xunit;
+
+namespace LeetCode.Tests.Additional
+{
+    public class Sorting_Test
+    {
+        private readonly Fixture _fixture = new();
+
+        private readonly IEnumerable<int[]> _arrays;
+
+        public Sorting_Test()
+        {
+            var random = new Random();
+
+            var arrays = new List<int[]> {new[] {6, 4, -3, 5}};
+
+            for (var i = 0; i < 5; i++)
+            {
+                var countOfItems = random.Next(1, 100);
+                arrays.Add(_fixture.CreateMany<int>(countOfItems).ToArray());
+            }
+
+            _arrays = arrays;
+        }
+
+        [Fact]
+        public void BubbleSortTest()
+        {
+            SortTest(Sorting.BubbleSorting);
+        }
+
+        [Fact]
+        public void SelectionSortTest()
+        {
+            SortTest(Sorting.SelectionSort);
+        }
+
+        [Fact]
+        public void InsertionSortTest()
+        {
+            SortTest(Sorting.InsertionSort);
+        }
+
+        private void SortTest(Action<int[]> sortingAction)
+        {
+            foreach (var array in _arrays)
+            {
+                sortingAction(array);
+                array.Should().BeInAscendingOrder();
+            }
+        }
+    }
+}
---
+++ LeetCode/Additional/Sorting.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Additional
+{
+    public static class Sorting
+    {
+        public static void BubbleSorting(int[] array)
+        {
+            for (var partIndex = array.Length - 1; partIndex >= 0; partIndex--)
+            {
+                for (var j = 0; j < partIndex; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        (array[j], array[j + 1]) = (array[j + 1], array[j]);
+                    }
+                }
+            }
+        }
+
+        public static void SelectionSort(int[] array)
+        {
+            for (var partIndex = array.Length - 1; partIndex >= 0; partIndex--)
+            {
+                var largerIndex = 0;
+
+                for (var i = 1; i <= partIndex; i++)
+                {
+                    if (array[i] > array[largerIndex])
+                    {
+                        largerIndex = i;
+                    }
+                }
+
+                (array[partIndex], array[largerIndex]) = (array[largerIndex], array[partIndex]);
+            }
+        }
+
+        public static void InsertionSort(int[] array)
+        {
+            for (var partIndex = 1; partIndex < array.Length; partIndex++)
+            {
+                var curUnsorted = array[partIndex];
+                
+                int i;
+                for (i = partIndex; i > 0 && array[i - 1] > curUnsorted; i--)
+                {
+                    array[i] = array[i - 1];
+                }
+
+                array[i] = curUnsorted;
+
+            }
+        }
+    }
+}
