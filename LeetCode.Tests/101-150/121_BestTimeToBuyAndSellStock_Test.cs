cffed1d9e2cce4586c9bcdfd010ab37802be0376
Paul Rozhkin <blackiiifox@gmail.com>
2022-03-30 23:30:12 +0300

    121 problem

---
+++ LeetCode.Tests/101-150/121_BestTimeToBuyAndSellStock_Test.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LeetCode._101_150
+{
+    public class _121_BestTimeToBuyAndSellStock_Test
+    {
+        [Fact]
+        public void MaxProfitTest_1()
+        {
+            var solution = new _121_BestTimeToBuyAndSellStock();
+            var result = solution.MaxProfit(new[] { 7, 1, 5, 3, 6, 4 });
+            Assert.Equal(5, result);
+        }
+
+        [Fact]
+        public void MaxProfitTest_2()
+        {
+            var solution = new _121_BestTimeToBuyAndSellStock();
+            var result = solution.MaxProfit(new[] { 7, 6, 4, 3, 1 });
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void MaxProfitTest_3()
+        {
+            var solution = new _121_BestTimeToBuyAndSellStock();
+            var result = solution.MaxProfit(new[] { 1, 2 });
+            Assert.Equal(1, result);
+        }
+    }
+}
---
+++ LeetCode/101-150/121_BestTimeToBuyAndSellStock.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode._101_150
+{
+    public class _121_BestTimeToBuyAndSellStock
+    {
+        // O(N)
+        public int MaxProfit(int[] prices)
+        {
+            var minPrice = prices[0];
+            var maxProfit = 0;
+
+            for (var i = 1; i < prices.Length; i++)
+            {
+                if (prices[i] > minPrice)
+                {
+                    maxProfit = Math.Max(maxProfit, prices[i] - minPrice);
+                }
+                else
+                {
+                    minPrice = prices[i];
+                }
+            }
+
+            return maxProfit;
+        }
+
+        // O(N^2)
+        //public int MaxProfit(int[] prices)
+        //{
+        //    var maxProfit = 0;
+        //    for (var i = 0; i < prices.Length; i++)
+        //    {
+        //        for (var j = i + 1; j < prices.Length; j++)
+        //        {
+        //            if (prices[j] - prices[i] > maxProfit)
+        //            {
+        //                maxProfit = prices[j] - prices[i];
+        //            }
+        //        }
+        //    }
+
+        //    return maxProfit;
+        //}
+    }
+}
