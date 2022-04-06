7b5b83270289108b5fb8c317320980c89c034197
Paul Rozhkin <blackiiifox@gmail.com>
2022-04-07 00:04:28 +0300

    238 problem "ProductOfArrayExceptSelf"

---
+++ LeetCode.Tests/201-250/238_ProductOfArrayExceptSelf_Test.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode._201_250;
+using LeetCode.Tests.Utils;
+using Xunit;
+
+namespace LeetCode.Tests._201_250
+{
+    public class _238_ProductOfArrayExceptSelf_Test
+    {
+        [Fact]
+        public void ProductExceptSelfTest_1()
+        {
+            var solution = new _238_ProductOfArrayExceptSelf();
+            var result = solution.ProductExceptSelf(new int[] { 1, 2, 3, 4 });
+            Assert.Equal(new int[] { 24, 12, 8, 6 }, result);
+        }
+
+        [Fact]
+        public void ProductExceptSelfTest_2()
+        {
+            var solution = new _238_ProductOfArrayExceptSelf();
+            var result = solution.ProductExceptSelf(new int[] { 0, 1, 2, 3, 4 });
+            Assert.Equal(new int[] { 24, 0, 0, 0, 0 }, result);
+        }
+
+        [Fact]
+        public void ProductExceptSelfTest_3()
+        {
+            var solution = new _238_ProductOfArrayExceptSelf();
+            var result = solution.ProductExceptSelf(new int[] { 1, 2, 3, 4, 0, 0 });
+            Assert.Equal(new int[] { 0, 0, 0, 0, 0, 0 }, result);
+        }
+    }
+}
---
+++ LeetCode/201-250/238_ProductOfArrayExceptSelf.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode._201_250
+{
+    public class _238_ProductOfArrayExceptSelf
+    {
+        /// <summary>
+        /// Time Complexity: O(n).
+        /// Space Complexity: O(1). 
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public int[] ProductExceptSelf(int[] nums)
+        {
+            var result = new int[nums.Length];
+
+            var product = 1;
+            result[0] = 1;
+            for (var i = 0; i < nums.Length - 1; i++)
+            {
+                product *= nums[i];
+                result[i + 1] = product;
+            }
+
+            product = nums[^1];
+            result[nums.Length - 2] *= product;
+            for (var i = nums.Length - 2; i > 0; i--)
+            {
+                product *= nums[i];
+                result[i - 1] *= product;
+            }
+
+            return result;
+        }
+
+        /*
+        /// <summary>
+        /// Time Complexity: O(n).
+        /// Space Complexity: O(n). 
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public int[] ProductExceptSelf(int[] nums)
+        {
+            var leftProduct = new int[nums.Length];
+            var rightProduct = new int[nums.Length];
+
+            var product = 1;
+            leftProduct[0] = 1;
+            for (var i = 0; i < nums.Length - 1; i++)
+            {
+                product *= nums[i];
+                leftProduct[i + 1] = product;
+            }
+
+            product = 1;
+            rightProduct[nums.Length - 1] = 1;
+            for (var i = nums.Length - 1; i > 0 ; i--)
+            {
+                product *= nums[i];
+                rightProduct[i - 1] = product;
+            }
+
+            var result = new int[nums.Length];
+            for (var i = 0; i < nums.Length; i++)
+            {
+                result[i] = leftProduct[i] * rightProduct[i];
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// O(N^2)
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public int[] ProductExceptSelf(int[] nums)
+        {
+            var result = new int[nums.Length];
+            for (var i = 0; i < nums.Length; i++)
+            {
+                result[i] = 1;
+
+                for (var j = 0; j < nums.Length; j++)
+                {
+                    if (i != j)
+                    {
+                        result[i] *= nums[j];
+                    }
+                }
+            }
+
+            return result;
+        }
+        */
+    }
+}
