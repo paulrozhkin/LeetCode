ac2d573da83f06c649d259a719c28bb0796d31cb
Paul Rozhkin <blackiiifox@gmail.com>
2022-04-09 23:09:48 +0300

    152 problem "MaximumProductSubarray"

---
+++ LeetCode.Tests/151-200/152_MaximumProductSubarray_Test.cs
@@ -0,0 +1,40 @@
+﻿using LeetCode._151_200;
+using Xunit;
+
+namespace LeetCode.Tests._151_200
+{
+    public class _152_MaximumProductSubarray_Test
+    {
+        [Fact]
+        public void MaxProductTest_1()
+        {
+            var solution = new _152_MaximumProductSubarray();
+            var result = solution.MaxProduct(new int[] { 2, 3, -2, 4 });
+            Assert.Equal(6, result);
+        }
+
+        [Fact]
+        public void MaxProductTest_2()
+        {
+            var solution = new _152_MaximumProductSubarray();
+            var result = solution.MaxProduct(new int[] { -2, 0, -1 });
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void MaxProductTest_3()
+        {
+            var solution = new _152_MaximumProductSubarray();
+            var result = solution.MaxProduct(new int[] { 1 });
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void MaxProductTest_4()
+        {
+            var solution = new _152_MaximumProductSubarray();
+            var result = solution.MaxProduct(new int[] { });
+            Assert.Equal(0, result);
+        }
+    }
+}
---
+++ LeetCode/151-200/152_MaximumProductSubarray.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace LeetCode._151_200
+{
+    public class _152_MaximumProductSubarray
+    {
+        /// <summary>
+        /// O(N)
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public int MaxProduct(int[] nums)
+        {
+            if (nums.Length == 0)
+            {
+                return 0;
+            }
+
+            // max positive product
+            // ending at the current position
+            int maxEndingHere = nums[0];
+
+            // min negative product ending
+            // at the current position
+            int minEndingHere = nums[0];
+
+            // Initialize overall max product
+            int maxSoFar = nums[0];
+
+            /* Traverse through the array.
+            the maximum product subarray ending at an index
+            will be the maximum of the element itself,
+            the product of element and max product ending previously
+            and the min product ending previously. */
+            for (var i = 1; i < nums.Length; i++)
+            {
+                var temp = Math.Max(Math.Max(nums[i], nums[i] * maxEndingHere), nums[i] * minEndingHere);
+                minEndingHere = Math.Min(Math.Min(nums[i], nums[i] * maxEndingHere), nums[i] * minEndingHere);
+                maxEndingHere = temp;
+                maxSoFar = Math.Max(maxSoFar, maxEndingHere);
+            }
+
+            return maxSoFar;
+        }
+    }
+}
