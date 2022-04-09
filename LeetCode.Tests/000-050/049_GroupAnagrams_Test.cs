758aeb50540d242e8dad03918f25847685f12455
Paul Rozhkin <blackiiifox@gmail.com>
2022-04-09 20:35:07 +0300

    49 problem "GroupAnagrams"

---
+++ LeetCode.Tests/000-050/049_GroupAnagrams_Test.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode._0_50;
+using LeetCode.Tests.Utils;
+using Xunit;
+
+namespace LeetCode.Tests._000_050
+{
+    public class _049_GroupAnagrams_Test
+    {
+        [Fact]
+        public void GroupAnagramsTest()
+        {
+            var input = new string[] {"eat", "tea", "tan", "ate", "nat", "bat"};
+
+            var solution = new _049_GroupAnagrams();
+            var result = solution.GroupAnagrams(input);
+
+            AssertHelper.AssertList(
+                new List<IList<string>>()
+                {
+                    new List<string>() {"eat", "tea", "ate"},
+                    new List<string>() {"tan", "nat"},
+                    new List<string>() {"bat"}
+                }, result);
+        }
+
+        [Fact]
+        public void GroupAnagramsTest_Empty()
+        {
+            var input = new string[] { };
+
+            var solution = new _049_GroupAnagrams();
+            var result = solution.GroupAnagrams(input);
+
+            Assert.Empty(result);
+        }
+    }
+}
---
+++ LeetCode/0-50/049_GroupAnagrams.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode._0_50
+{
+    public class _049_GroupAnagrams
+    {
+        public IList<IList<string>> GroupAnagrams(string[] strs)
+        {
+            return strs
+                .GroupBy(str => new string(str.OrderBy(ch => ch).ToArray()))
+                .Select(x => (IList<string>) x.ToList())
+                .ToList();
+        }
+    }
+}
