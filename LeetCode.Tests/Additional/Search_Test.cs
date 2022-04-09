6a6fb94fd322ca7a8b05c7b7fb43838a313260b7
Paul Rozhkin <blackiiifox@gmail.com>
2022-04-09 12:08:03 +0300

    additional (binary search)

---
+++ LeetCode.Tests/Additional/Search_Test.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoFixture;
+using LeetCode.Additional;
+using Xunit;
+
+namespace LeetCode.Tests.Additional
+{
+    public class Search_Test
+    {
+        [Fact]
+        public void BinarySearchTest()
+        {
+            // arrange
+            var sortedPositives = new[] { -15, - 6, 29, 93, 142, 165};
+            
+            // act & asserts
+            Assert.Equal(4, Search.BinarySearch(sortedPositives, 142));
+            Assert.Equal(2, Search.BinarySearch(sortedPositives, 29));
+            Assert.Equal(3, Search.BinarySearch(sortedPositives, 93));
+            Assert.Equal(0, Search.BinarySearch(sortedPositives, -15));
+            Assert.Equal(5, Search.BinarySearch(sortedPositives, 165));
+            Assert.Equal(-1, Search.BinarySearch(sortedPositives, 300));
+            Assert.Equal(-1, Search.BinarySearch(sortedPositives, -300));
+
+           
+        }
+
+        [Fact]
+        public void BinarySearchRandomArrayTest()
+        {
+            // arrange
+            var randomBigArray = new Fixture().CreateMany<int>(999).ToArray();
+            Array.Sort(randomBigArray);
+            var randomIndex = new Random().Next(0, randomBigArray.Length);
+
+            // act & asserts
+            Assert.Equal(randomIndex, Search.BinarySearch(randomBigArray, randomBigArray[randomIndex]));
+        }
+
+        [Fact]
+        public void BinarySearchRecursionTest()
+        {
+            // arrange
+            var sortedPositives = new[] { -15, -6, 29, 93, 142, 165 };
+
+            // act & asserts
+            Assert.Equal(4, Search.BinarySearchRecursion(sortedPositives, 142));
+            Assert.Equal(2, Search.BinarySearchRecursion(sortedPositives, 29));
+            Assert.Equal(3, Search.BinarySearchRecursion(sortedPositives, 93));
+            Assert.Equal(0, Search.BinarySearchRecursion(sortedPositives, -15));
+            Assert.Equal(5, Search.BinarySearchRecursion(sortedPositives, 165));
+            Assert.Equal(-1, Search.BinarySearchRecursion(sortedPositives, 300));
+            Assert.Equal(-1, Search.BinarySearchRecursion(sortedPositives, -300));
+
+
+        }
+
+        [Fact]
+        public void BinarySearchRecursionRandomArrayTest()
+        {
+            // arrange
+            var randomBigArray = new Fixture().CreateMany<int>(999).ToArray();
+            Array.Sort(randomBigArray);
+            var randomIndex = new Random().Next(0, randomBigArray.Length);
+
+            // act & asserts
+            Assert.Equal(randomIndex, Search.BinarySearchRecursion(randomBigArray, randomBigArray[randomIndex]));
+        }
+    }
+}
---
+++ LeetCode/Additional/Search.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Additional
+{
+    public class Search
+    {
+        public static int BinarySearch(int[] array, int value)
+        {
+            var low = 0;
+            var high = array.Length;
+
+            while (low < high)
+            {
+                var middle = (high + low) / 2;
+
+                if (array[middle] == value)
+                    return middle;
+                if (array[middle] > value)
+                    high = middle;
+                else
+                    low = middle + 1;
+            }
+
+            return -1;
+        }
+
+        public static int BinarySearchRecursion(int[] array, int value)
+        {
+            return BinarySearchRecursionFromTo(0, array.Length);
+
+            int BinarySearchRecursionFromTo(int low, int high)
+            {
+                if (low >= high)
+                {
+                    return -1;
+                }
+
+                var mid = (low + high) / 2;
+                if (array[mid] == value)
+                    return mid;
+                if (array[mid] > value)
+                    return BinarySearchRecursionFromTo(low, mid);
+                else
+                {
+                    return BinarySearchRecursionFromTo(mid + 1, high);
+                }
+            }
+        }
+    }
+}
