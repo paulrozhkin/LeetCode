f3f561e1da6cc9862aad6c57527eeeed4f282473
Paul Rozhkin <blackiiifox@gmail.com>
2022-04-14 16:26:35 +0300

    206 problem "ReverseLinkedList"

---
+++ LeetCode.Tests/201-250/206_ReverseLinkedList_Test.cs
@@ -0,0 +1,19 @@
+﻿using LeetCode._201_250;
+using LeetCode.Tests.Utils;
+using Xunit;
+
+namespace LeetCode.Tests._201_250
+{
+    public class _206_ReverseLinkedList_Test
+    {
+        [Fact]
+        public void ReverseListTest()
+        {
+            var head = Generator.GenerateList(new int[] { 1, 2, 3, 4, 5 });
+
+            var solution = new _206_ReverseLinkedList();
+            var newHead = solution.ReverseList(head);
+            AssertHelper.AssertLinkList(new int[] { 5, 4, 3, 2, 1 }, newHead);
+        }
+    }
+}
---
+++ LeetCode/201-250/206_ReverseLinkedList.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using LeetCode.Models;
+
+namespace LeetCode._201_250
+{
+    public class _206_ReverseLinkedList
+    {
+        public ListNode ReverseList(ListNode head)
+        {
+            return ReverseListRecursive(head);
+
+            ListNode ReverseListRecursive(ListNode headInner)
+            {
+                if (headInner?.next == null)
+                    return headInner;
+
+                var rest = ReverseListRecursive(headInner.next);
+                headInner.next.next = headInner;
+
+                headInner.next = null;
+                return rest;
+            }
+        }
+
+
+        /*
+        /// <summary>
+        /// Time: O(N)
+        /// Space: O(1)
+        /// </summary>
+        /// <param name="head"></param>
+        /// <returns></returns>
+        public ListNode ReverseList(ListNode head)
+        {
+            ListNode prevNode = null;
+            ListNode currentNode = head;
+
+            while (currentNode != null)
+            {
+                var nextNode = currentNode.next;
+                currentNode.next = prevNode;
+                prevNode = currentNode;
+                currentNode = nextNode;
+            }
+
+            return prevNode;
+        }
+
+        /// <summary>
+        /// Time: O(N)
+        /// Space: O(N)
+        /// </summary>
+        /// <param name="head"></param>
+        /// <returns></returns>
+        public ListNode ReverseList(ListNode head)
+        {
+            if (head == null)
+            {
+                return null;
+            }
+
+            if (head.next == null)
+            {
+                return head;
+            }
+
+            var references = new List<ListNode>();
+            var item = head;
+            while (item != null)
+            {
+                references.Add(item);
+                item = item.next;
+            }
+
+            for (var i = references.Count - 1; i > 0; i--)
+            {
+                references[i].next = references[i - 1];
+            }
+
+            head.next = null;
+
+            return references[^1];
+        }
+        */
+    }
+}
