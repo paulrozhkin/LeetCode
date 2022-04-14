fc9cef46fe74aa8219cdc4b5e4d6eb51eb88cca2
Paul Rozhkin <blackiiifox@gmail.com>
2022-04-14 16:57:21 +0300

    141 problem "LinkedListCycle"

---
+++ LeetCode.Tests/101-150/141_LinkedListCycle_Test.cs
@@ -0,0 +1,41 @@
+﻿using LeetCode._101_150;
+using LeetCode.Tests.Utils;
+using Xunit;
+
+namespace LeetCode.Tests._101_150
+{
+    public class _141_LinkedListCycle_Test
+    {
+        [Fact]
+        public void HasCycle_1()
+        {
+            var root = Generator.GenerateList(new int[] { 3, 2, 0, -4 });
+            root.next.next.next.next = root.next;
+
+            var solution = new _141_LinkedListCycle();
+            var result = solution.HasCycle(root);
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void HasCycle_2()
+        {
+            var root = Generator.GenerateList(new int[] { 1, 2 });
+            root.next.next = root;
+
+            var solution = new _141_LinkedListCycle();
+            var result = solution.HasCycle(root);
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void HasCycle_3()
+        {
+            var root = Generator.GenerateList(new int[] { 1 });
+
+            var solution = new _141_LinkedListCycle();
+            var result = solution.HasCycle(root);
+            Assert.False(result);
+        }
+    }
+}
---
+++ LeetCode/101-150/141_LinkedListCycle.cs
@@ -0,0 +1,55 @@
+﻿using LeetCode.Models;
+
+namespace LeetCode._101_150
+{
+    public class _141_LinkedListCycle
+    {
+        /// <summary>
+        /// Floyd's tortoise and hare
+        /// Space: O(1)
+        /// </summary>
+        /// <param name="head"></param>
+        /// <returns></returns>
+        public bool HasCycle(ListNode head)
+        {
+            var fast = head;
+            var slow = head;
+            while (fast != null && fast.next != null)
+            {
+                fast = fast.next.next;
+                slow = slow.next;
+
+                if (fast == slow)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        /*
+        /// <summary>
+        /// Space: O(N)
+        /// </summary>
+        /// <param name="head"></param>
+        /// <returns></returns>
+        public bool HasCycle(ListNode head)
+        {
+            var nodes = new List<ListNode>();
+            while (head != null)
+            {
+                if (nodes.Contains(head))
+                {   
+                    return true;
+                }
+
+                nodes.Add(head);
+                head = head.next;
+            }
+
+            return false;
+        }
+        */
+    }
+}
