fd4d1d829d71eb3385b3cc6e56f36c38d4615668
Paul Rozhkin <blackiiifox@gmail.com>
2022-04-18 13:24:02 +0300

    additional (hash symbol table)

---
+++ LeetCode.Tests/Additional/ChainHashSetTests.cs
@@ -0,0 +1,138 @@
+﻿using System.Linq;
+using LeetCode.Additional;
+using Xunit;
+
+namespace LeetCode.Tests.Additional
+{
+    public class ChainHashSetTests
+    {
+        [Fact]
+        public void AddWithDefaultCapacityTest()
+        {
+            // arrange
+            var chainHashSet = new ChainHashSet<int, int>();
+
+            // act
+            chainHashSet.Add(0, 0);
+            chainHashSet.Add(1, 1);
+            chainHashSet.Add(2, 2);
+            chainHashSet.Add(3, 3);
+
+            // assert
+            Assert.Equal(4, chainHashSet.Count);
+            Assert.True(chainHashSet.Contains(2));
+        }
+
+        [Fact]
+        public void AddWithResizeCapacityTest()
+        {
+            // arrange
+            var chainHashSet = new ChainHashSet<int, int>();
+
+            // act
+            const int count = 100;
+            for (var i = 0; i < count; i++)
+            {
+                chainHashSet.Add(i, i);
+            }
+
+            // assert
+            Assert.Equal(count, chainHashSet.Count);
+            Assert.True(chainHashSet.Contains(25));
+        }
+
+        [Fact]
+        public void ContainsTest()
+        {
+            // arrange
+            var chainHashSet = new ChainHashSet<int, int>();
+
+            // act
+            chainHashSet.Add(0, 0);
+            chainHashSet.Add(1, 1);
+
+            // assert
+            Assert.True(chainHashSet.Contains(1));
+        }
+
+        [Fact]
+        public void NotContainsTest()
+        {
+            // arrange
+            var chainHashSet = new ChainHashSet<int, int>();
+
+            // act
+            chainHashSet.Add(0, 0);
+            chainHashSet.Add(1, 1);
+
+            // assert
+            Assert.False(chainHashSet.Contains(2));
+        }
+
+        [Fact]
+        public void GetTest()
+        {
+            // arrange
+            var chainHashSet = new ChainHashSet<int, int>();
+
+            // act
+            chainHashSet.Add(0, 0);
+            chainHashSet.Add(1, 1);
+            chainHashSet.Add(2, 2);
+            chainHashSet.Add(3, 3);
+            chainHashSet.Add(4, 8);
+            chainHashSet.Add(5, 5);
+            chainHashSet.Add(6, 6);
+
+            // assert
+            Assert.Equal(8, chainHashSet.Get(4));
+        }
+
+        [Fact]
+        public void KeysTest()
+        {
+            // arrange
+            var chainHashSet = new ChainHashSet<int, int>();
+
+            // act
+            chainHashSet.Add(0, 0);
+            chainHashSet.Add(1, 1);
+            chainHashSet.Add(2, 2);
+            chainHashSet.Add(3, 3);
+            chainHashSet.Add(4, 8);
+            chainHashSet.Add(5, 5);
+            chainHashSet.Add(6, 6);
+
+            // assert
+            var sortedKeys = chainHashSet.Keys().ToList();
+            sortedKeys.Sort();
+            Assert.Equal(new[] {0, 1, 2, 3, 4, 5, 6}, sortedKeys);
+        }
+
+        [Fact]
+        public void RemoveTest()
+        {
+            // arrange
+            var chainHashSet = new ChainHashSet<int, int>();
+
+            // act
+            chainHashSet.Add(0, 0);
+            chainHashSet.Add(1, 1);
+            chainHashSet.Add(2, 2);
+            chainHashSet.Add(3, 3);
+            chainHashSet.Add(4, 4);
+            chainHashSet.Add(5, 5);
+            chainHashSet.Add(6, 6);
+
+            chainHashSet.Remove(1);
+            chainHashSet.Remove(3);
+            chainHashSet.Remove(5);
+
+            // assert
+            Assert.Equal(4, chainHashSet.Count);
+            var sortedKeys = chainHashSet.Keys().ToList();
+            sortedKeys.Sort();
+            Assert.Equal(new[] {0, 2, 4, 6}, sortedKeys);
+        }
+    }
+}
---
+++ LeetCode/Additional/ChainHashSet.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Additional
+{
+    public class ChainHashSet<TKey, TValue>
+    {
+        private SequentialSearchSymbolTable<TKey, TValue>[] _chains;
+
+        private const int DefaultCapacity = 4;
+
+        public int Count { get; private set; }
+
+        public int Capacity { get; private set; }
+
+        public ChainHashSet(int capacity)
+        {
+            Capacity = capacity;
+            _chains = new SequentialSearchSymbolTable<TKey, TValue>[capacity];
+            for (int i = 0; i < capacity; i++)
+            {
+                _chains[i] = new SequentialSearchSymbolTable<TKey, TValue>();
+            }
+        }
+
+        public ChainHashSet() : this(DefaultCapacity)
+        {
+        }
+
+        private int Hash(TKey key)
+        {
+            return (key.GetHashCode() & 0x7fffffff) % Capacity;
+        }
+
+        public TValue Get(TKey key)
+        {
+            if (key == null)
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+
+            var index = Hash(key);
+            if (_chains[index].TryGet(key, out var value))
+            {
+                return value;
+            }
+
+            throw new ArgumentException("Keys not found");
+        }
+
+        public bool Contains(TKey key)
+        {
+            if (key == null)
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+
+            var index = Hash(key);
+            return _chains[index].Contains(key);
+        }
+
+        public bool Remove(TKey key)
+        {
+            if (key == null)
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+
+            var index = Hash(key);
+            if (_chains[index].Remove(key))
+            {
+                Count--;
+                return true;
+            }
+
+            return false;
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            if (key == null)
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+
+            if (value == null)
+            {
+                Remove(key);
+                return;
+            }
+
+            if (Count >= 10 * Capacity)
+            {
+                Resize(2 * Capacity);
+            }
+
+            var index = Hash(key);
+
+            if (!_chains[index].Contains(key))
+            {
+                Count++;
+            }
+
+            _chains[index].Add(key, value);
+        }
+
+        private void Resize(int newSize)
+        {
+            var newChain = new ChainHashSet<TKey, TValue>(newSize);
+
+            foreach (var chain in _chains)
+            {
+                foreach (var key in chain.Keys())
+                {
+                    if (chain.TryGet(key, out var value))
+                    {
+                        newChain.Add(key, value);
+                    }
+                }
+            }
+
+            Capacity = newChain.Capacity;
+            Count = newChain.Count;
+            _chains = newChain._chains;
+        }
+
+        public IEnumerable<TKey> Keys()
+        {
+            for (var i = 0; i < Capacity; i++)
+            {
+                foreach (var key in _chains[i].Keys())
+                {
+                    yield return key;
+                }
+            }
+        }
+    }
+}
