3e75dc291e3fd71af75e7ea390fa826f7dbae587
Paul Rozhkin <blackiiifox@gmail.com>
2022-04-19 22:03:25 +0300

    additional (algorithm SieveOfEratosthenes)

---
+++ LeetCode.Tests/Additional/SieveOfEratosthenesTest.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode.Additional;
+using Xunit;
+
+namespace LeetCode.Tests.Additional
+{
+    public class SieveOfEratosthenesTest
+    {
+        [Fact]
+        public void PrimeNumbersTest()
+        {
+            // arrange
+            var n = 30;
+            var expected = new[] {2, 3, 5, 7, 11, 13, 17, 19, 23, 29};
+            var sieveOfEratosthenes = new SieveOfEratosthenes();
+
+            // act
+            var primes = sieveOfEratosthenes.PrimeNumbers(n);
+
+            // assert
+            Assert.Equal(expected, primes);
+        }
+    }
+}
---
+++ LeetCode/Additional/SieveOfEratosthenes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Additional
+{
+    public class SieveOfEratosthenes
+    {
+        public IEnumerable<int> PrimeNumbers(int n)
+        {
+            var mask = new bool[n];
+
+            for (var p = 2; p < n; p++)
+            {
+                if (mask[p])
+                    continue;
+
+                yield return p;
+
+                for (var i = p * p; i < n; i += p)
+                    mask[i] = true;
+            }
+        }
+    }
+}
